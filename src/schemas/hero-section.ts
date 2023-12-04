38cb75eca719d8a61c77423d9477338d176f95f7
genuineAB <bamigboyeabiola@yahoo.com>
2023-12-04 17:28:16 +0100

    Added project to hero-section

--- src/schemas/hero-section.ts
+++ src/schemas/hero-section.ts
@@ -3,7 +3,9 @@ import Joi from 'joi';
 export const heroSectionSchema = Joi.object({
   header: Joi.string().required().default("Women's Board"),
   subHeader: Joi.string().allow('').optional(),
-  page: Joi.string().valid('about', 'home', 'un-collaboration').required(),
+  page: Joi.string()
+    .valid('about', 'home', 'un-collaboration', 'project')
+    .required(),
   imageURL: Joi.array().when('page', {
     is: 'home',
     then: Joi.array().items(Joi.string()).required(),
